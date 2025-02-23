ff3b3219f8d687c1aaef20797fda0ef770a8a652
wang-yuancheng <yuanchengntu@gmail.com>
2025-02-23 13:02:07 +0800

    add option for human input

--- script.js
+++ script.js
@@ -7,5 +7,20 @@ function getComputerChoice() {
   } else {
     return "Scissors";
   }
-  
+}
+
+function getHumanChoice() {
+  let HumanChoice = prompt("Choose either Rock, Paper or Scissors.");
+  if (HumanChoice.toLowerCase() === "rock") {
+    console.log("You chose Rock");
+  } else if (HumanChoice.toLowerCase() === "paper") {
+    console.log("You chose Paper");
+  } else if (HumanChoice.toLowerCase() === "scissors") {
+    HumanChoice.toLowerCase() === "scissors";
+    console.log("You chose Scissors");
+  } else {
+    alert("Choose a valid choice!");
+    getHumanChoice();
+  }
+  return HumanChoice;
 }

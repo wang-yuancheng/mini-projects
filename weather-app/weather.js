ba1d10ff6268c445957622fac20bef2d84c332c4
wang-yuancheng <yuanchengntu@gmail.com>
2025-06-12 17:38:58 +0800

    Add javascript logic

---
+++ weather-app/weather.js
@@ -0,0 +1,70 @@
+import axios from "axios";
+
+export function getWeather(lat, lon, timezone) {
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&hourly=temperature_2m,apparent_temperature,weather_code,precipitation,wind_speed_10m&current=temperature_2m,weather_code,wind_speed_10m&timeformat=unixtime&wind_speed_unit=mph&precipitation_unit=inch",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone,
+        },
+      }
+    )
+    .then(({ data }) => {
+      return {
+        current: parseCurrentWeather(data),
+        daily: parseDailyWeather(data),
+        hourly: parseHourlyWeather(data),
+      };
+    });
+}
+
+function parseCurrentWeather({ current, daily }) {
+  const {
+    temperature_2m: currentTemp,
+    wind_speed_10m: windSpeed,
+    weather_code: iconCode,
+  } = current;
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  };
+}
+
+function parseDailyWeather({ daily }) {
+  return daily.time.map((time, index) => {
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weather_code[index],
+      maxTemp: Math.round(daily.temperature_2m_max[index]),
+    };
+  });
+}
+
+function parseHourlyWeather({ hourly, current }) {
+    return hourly.time.map((time, index) => {
+        return {
+            timestamp: time * 1000,
+            iconCode: hourly.weather_code[index],
+            temp: Math.round(hourly.temperature_2m[index]),
+            feelsLike: Math.round(hourly.apparent_temperature[index]),
+            windSpeed: Math.round(hourly.wind_speed_10m[index]),
+            precip: Math.round(hourly.precipitation[index] * 100) /100,
+        }
+    }).filter(({ timestamp }) => timestamp >= current.time * 1000)
+}

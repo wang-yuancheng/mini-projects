11684bb2ba0a483a04b5c7ce18a653997db74d30
wang-yuancheng <yuanchengntu@gmail.com>
2025-07-09 04:24:10 +0800

    Add animations

--- nextjs-landing-page/src/sections/CallToAction.tsx
+++ nextjs-landing-page/src/sections/CallToAction.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import ArrowRight from "@/assets/arrow-right.svg";
 import starImage from "@/assets/star.png";
 import springImage from "@/assets/spring.png";
-import Image from "next/image";
-import { spring } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -14,13 +27,20 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={starImage}
+          <motion.img
+            src={starImage.src}
             alt="Star Image"
             width={360}
             className="hidden md:block absolute -left-[350px] -top-[137px]"
+            style={{ translateY }}
           />
-          <Image src={springImage} alt="Spring Image" width={360} className="hidden md:block absolute -right-[331px] -top-[19px]"/>
+          <motion.img
+            src={springImage.src}
+            alt="Spring Image"
+            width={360}
+            className="hidden md:block absolute -right-[331px] -top-[19px]"
+            style={{ translateY }}
+          />
         </div>
         <div className="flex gap-2 mt-10 justify-center">
           <button className="btn btn-primary">Get for free</button>
